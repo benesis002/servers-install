ba6efef4a4e96b024353c3b4a14b981f5a28ef04
Benjamin Kaminski <benesis002@outlook.com>
2017-10-14 12:33:59 +0200

    Update deploy-plex.sh

--- deploy-plex.sh
+++ deploy-plex.sh
@@ -1,10 +1,19 @@
 docker run \
 -d \
 --name plex \
---network=host \
---restart always \
+-p 32400:32400/tcp \
+-p 3005:3005/tcp \
+-p 8324:8324/tcp \
+-p 32469:32469/tcp \
+-p 1900:1900/udp \
+-p 32410:32410/udp \
+-p 32412:32412/udp \
+-p 32413:32413/udp \
+-p 32414:32414/udp \
 -e TZ="Europe/Paris" \
 -e PLEX_CLAIM="claim-6sE1uFkUVrPzwebzL7kq" \
+-e ADVERTISE_IP="http://192.168.0.100:32400/" \
+-h debian-lille \
 -v /mnt/volumeI/data/plextemp:/config \
 -v /tmp:/transcode \
 -v /mnt/volumeI/data/multimedia:/data \
