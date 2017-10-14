07c5b9174a62b0e05a7d47d8e9a5062694d73a27
Benjamin Kaminski <benesis002@outlook.com>
2017-10-14 12:52:15 +0200

    Update deploy-jeedom.sh

--- deploy-jeedom.sh
+++ deploy-jeedom.sh
@@ -1,1 +1,1 @@
-docker run --name jeedom-server --link localdb:mysql --privileged --privileged -v /dev/bus/usb/001/004:/dev/ttyacm0 -v /opt/jeedom:/var/www/html -v /mnt/backup:/mnt/backup -e ROOT_PASSWORD=$1 -p 80:80 -p 443:443 --network="dedicatedjeedomdb" jeedom/jeedom
+docker run --name jeedom-server --link localdb:mysql --privileged -v /dev/bus/usb/001/004:/dev/ttyacm0 -v /opt/jeedom:/var/www/html -v /mnt/volumeI/backup:/mnt/backup -e ROOT_PASSWORD=$1 -p 80:80 -p 443:443 --network="dedicatedjeedomdb" jeedom/jeedom
