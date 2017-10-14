1a2e397c13b38989ed6c5a0fa62b702a000f8ec5
Benjamin Kaminski <benesis002@outlook.com>
2017-10-14 12:31:27 +0200

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -3,4 +3,4 @@ echo 'deb https://apt.dockerproject.org/repo debian-stretch main' >> /etc/apt/so
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 apt-get update
 apt-get install -y docker-engine
-docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer
+docker run --name webui -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v /opt/portainer:/data portainer/portainer --restart always
