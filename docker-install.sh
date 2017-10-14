81f2f96f85b430802941471172487e48e0fb5288
Benjamin Kaminski <benesis002@outlook.com>
2017-10-14 12:30:23 +0200

    Create docker-install.sh

---
+++ docker-install.sh
@@ -0,0 +1,6 @@
+apt-get install -y apt-transport-https dirmngr
+echo 'deb https://apt.dockerproject.org/repo debian-stretch main' >> /etc/apt/sources.list
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-get update
+apt-get install -y docker-engine
+docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer
