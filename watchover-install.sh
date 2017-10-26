2e7c3d64c2d0aa5376964c744af25a0c5ff6834c
Benjamin Kaminski <benesis002@outlook.com>
2017-10-26 23:04:00 +0200

    Update watchover-install.sh

--- watchover-install.sh
+++ watchover-install.sh
@@ -1,1 +1,1 @@
-docker run -d --name watchtower -v /var/run/docker.sock:/var/run/docker.sock v2tec/watchtower --schedule 30 2 * * * --cleanup
+docker run -d --name watchtower -v /var/run/docker.sock:/var/run/docker.sock v2tec/watchtower --schedule '0 3 0 * * *' --cleanup --debug
